3b6a6fa1215e6a254f7a5d2c99849de9bc39b1cc
Programação Disposiivos Móveis <alunoandroid@fatec.local>
2023-04-12 12:43:33 -0300

    terceiro commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,7 +6,39 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: Stack(
+          children: <Widget> [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(begin: Alignment.topLeft, colors: [
+                  Color.fromARGB(255, 32, 181, 240),
+                  Color.fromARGB(255, 29, 147, 226),
+                  Color.fromARGB(255, 69, 114, 197),
+                ]),
+              ),
+            ),
+            Image.asset('assets/images/undraw_Login.png', height: 180, width: double.maxFinite,),
+            Positioned(
+              child: Container(
+                margin: EdgeInsets.only(top: 180),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  ),
+                ),
+
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
